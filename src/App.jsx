a8f930fb0d41d850b1f9a913d8d6ab3567195f8d
bouidiamaamarmustapha-creator <bouidiamaamarmustapha@gmail.com>
2025-09-08 02:50:08 +0100

    Create missing main.jsx and App.jsx files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Algerian Judo Federation</h1>
+        <p>Manage Judo Club</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
